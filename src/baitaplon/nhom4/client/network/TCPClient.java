dae8f7764c82f45e1f40b46801e5b6ffa6cd8400
quanghung2k4 <quanghungbg2021@gmail.com>
2025-10-29 08:56:49 +0700

    update no-pull

--- src/baitaplon/nhom4/client/network/TCPClient.java
+++ src/baitaplon/nhom4/client/network/TCPClient.java
@@ -83,6 +83,11 @@ public class TCPClient {
                             dashBoardController.handleGameStart((GameStartDTO) message.getData());
                         }
                         break;
+                    case "opponent_scored":
+                        if (activeGameScreen != null) {
+                            activeGameScreen.handleOpponentScored(message.getContent());
+                        }
+                        break;
                     case "game_end":
                         if (activeGameScreen != null) {
                             activeGameScreen.handleGameEnd(message.getContent());
